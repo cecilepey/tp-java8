70f1865a457857235b99302d3c0d66364f3567b8
Cecile Peyras <peyrascecile@gmail.com>
2019-05-23 17:13:05 +0200

    TP function

--- src/test/java/java8/ex03/Function_03_Test.java
+++ src/test/java/java8/ex03/Function_03_Test.java
@@ -1,38 +1,46 @@
 package java8.ex03;
 
-import java8.data.Person;
+import java.util.function.BinaryOperator;
+
 import org.junit.Test;
 
-import java.util.function.BinaryOperator;
+import java8.data.Person;
 
 /**
  * Exercice 03 - java.util.function.BinaryOperator
  */
 public class Function_03_Test {
 
-    //  tag::makeAChild[]
-    // TODO Compléter la fonction makeAChild
-    // TODO l'enfant possède le nom du père
-    // TODO l'enfant possède le prenom "<PRENOM_PERE> <PRENOM_MERE>"
-    // TODO l'age de l'enfant est 0
-    // TODO le mot de passe de l'enfant est null
-    BinaryOperator<Person> makeAChild = null;
-    //  end::makeAChild[]
+	// tag::makeAChild[]
+	// TODO Compléter la fonction makeAChild
+	// TODO l'enfant possède le nom du père
+	// TODO l'enfant possède le prenom "<PRENOM_PERE> <PRENOM_MERE>"
+	// TODO l'age de l'enfant est 0
+	// TODO le mot de passe de l'enfant est null
+	BinaryOperator<Person> makeAChild = (papa, maman) -> {
+		Person enfant = new Person();
+		enfant.setFirstname(papa.getFirstname() + " " + maman.getFirstname());
+		enfant.setLastname(papa.getLastname());
+		enfant.setAge(0);
+		enfant.setPassword(null);
+		return enfant;
 
+	};
+	// end::makeAChild[]
 
-    @Test
-    public void test_makeAChild() throws Exception {
+	@Test
+	public void test_makeAChild() throws Exception {
 
-        Person father = new Person("John", "France", 25, "johndoe");
-        Person mother = new Person("Aline", "Lebreton", 22, "alino");
+		Person father = new Person("John", "France", 25, "johndoe");
+		Person mother = new Person("Aline", "Lebreton", 22, "alino");
 
-        // TODO compléter le test pour qu'il soit passant
-        Person child = null;
+		// TODO compléter le test pour qu'il soit passant
+		Person child = makeAChild.apply(father, mother);
 
-        assert child.getFirstname().equals("John Aline");
-        assert child.getLastname().equals("France");
-        assert child.getAge().equals(0);
-        assert child.getPassword() == null;
-    }
+		assert child.getFirstname().equals("John Aline");
+		assert child.getLastname().equals("France");
+		assert child.getAge().equals(0);
+		assert child.getPassword() == null;
+	}
 
 }
